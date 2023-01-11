94d5355b6d54fe485e58c1e69fb09b33724dd987
subbuveera772 <subbuveera772@gmail.com>
2023-01-11 21:47:33 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    topic_name=models.CharField(max_length=100)
+
+class Webpage(models.Model):
+    topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
